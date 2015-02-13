539bd1c27bbcb033a9ef4f97fbc12a27e06b869e
aghosh47 <aghosh@kreeti.com>
2015-02-13 20:39:50 +0530

    who knows what the fuck happened

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 var express = require('express');
+var http = require('http');
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var app = express();
+var server = http.Server(app);
+var io = require('socket.io')(server);
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var rooms = require('./routes/rooms');
+app.set('port', 8000);
 
-var app = express();
+server.listen(app.get('port'), function() {
+  console.log("Boo");
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,6 +32,9 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/rooms', rooms);
 
+app.get('/index', function(req, res, next) {
+  res.render("index");
+})
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
@@ -58,4 +67,15 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+io.of('/rooms').on('connection', function(socket) {
+  socket.join('/')
+  console.log("user joined php room");
+  socket.emit('pong', { number: 0})
+  socket.on('ping', function(data) {
+    console.log(data)
+    socket.nickname = data.nickname
+    socket.room = data.room_id
+    //socket.join(data.room_id)
+
+  });
+});
--- libs/io.js
+++ libs/io.js
@@ -1,16 +1,3 @@
 var io = require('socket.io')();
 
-io.of('/rooms').on('connection', function(socket) {
-  console.log("user joined php room");
-  socket.emit('pong', { number: 0})
-  socket.on('ping', function(data) {
-    console.log(data)
-    socket.nickname = data.nickname
-    socket.room = data.room_id
-
-    socket.join(data.room_id)
-
-  });
-});
-
 module.exports = io
